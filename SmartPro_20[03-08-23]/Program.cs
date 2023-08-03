1ebd2526b01297d0a2dcd2eaca737d98e0a68a6e
lesponns <adamadakadam1903@gmail.com>
2023-08-03 19:43:35 +0300

    Add project files.

---
+++ SmartPro_20[03-08-23]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_20_03_08_23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string _username = "Lesponns", _username_inp;
+            string _password = "ZeroSix", _password_inp;
+
+            do
+            {
+                Console.WriteLine("Login Interface");
+                Console.WriteLine(" ");
+
+                Console.Write("UserName: ");
+                _username_inp = Console.ReadLine();
+
+                Console.Write("Password: ");
+                _password_inp = Console.ReadLine();
+                Console.Clear();
+            } while (_username_inp != _username || _password_inp != _password);
+
+            Console.WriteLine("Welcome, " + _username_inp);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
